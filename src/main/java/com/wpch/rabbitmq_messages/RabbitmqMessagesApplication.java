83f0c117db9236d1b6cb149f6387527e7cf6828f
wagnerpch <w.chequeleiro@gmail.com>
2025-04-03 06:33:10 -0300

    feat: estrutura inicial
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/wpch/rabbitmq_messages/RabbitmqMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.wpch.rabbitmq_messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wpch/rabbitmq_messages/RabbitmqMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpch.rabbitmq_messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
